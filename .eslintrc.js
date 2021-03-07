e1829065edbe5451a979fcd5ca7962051fb353f0
Nate Clark <n8@n8io.com>
2021-03-07 17:52:53 -0500

    🚨 Add linting and stylistic config

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+const path = require('path');
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+  ],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'import',
+    'import-helpers',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+    'jest',
+    'sort-keys-fix',
+  ],
+  rules: {
+    'arrow-body-style': ['warn', 'as-needed'],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        alphabetize: { order: 'asc' },
+        groups: ['module', '/^modules/', 'parent', ['sibling', 'index']],
+        newlinesBetween: 'never',
+      },
+    ],
+    'no-multiple-empty-lines': ['warn', { max: 1 }],
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'object-shorthand': ['warn', 'always'],
+    'react/jsx-sort-default-props': ['warn'],
+    'react/jsx-sort-props': ['warn'],
+    'react/react-in-jsx-scope': 'off',
+    'react/sort-prop-types': ['warn'],
+    'sort-keys-fix/sort-keys-fix': 'warn',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: [path.resolve(__dirname, 'src')],
+      },
+    },
+    react: { version: 'detect' },
+  },
+};
