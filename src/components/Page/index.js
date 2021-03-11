cb35dd5cdb0bcafcb17bd7fbe983c9ceb0132f2e
Nate Clark <n8@n8io.com>
2021-03-10 19:58:15 -0500

    ✨ Add app `Page` for all routes to leverage

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,11 +1,21 @@
-import { Layout, Main } from 'components/Layout';
+import { Header, Layout, Main } from 'components/Layout';
+import { useSnood } from 'hooks/useSnood';
 import { node } from 'prop-types';
+import { Helmet } from 'react-helmet-async';
 
-const Page = ({ children }) => (
-  <Layout>
-    <Main>{children}</Main>
-  </Layout>
-);
+const Page = ({ children }) => {
+  const snood = useSnood();
+
+  return (
+    <Layout>
+      <Helmet>
+        <link href={snood} rel="icon" sizes="32x32" type="image/png" />
+      </Helmet>
+      <Header />
+      <Main>{children}</Main>
+    </Layout>
+  );
+};
 
 Page.propTypes = {
   children: node.isRequired,
--- src/components/Page/index.spec.js
+++ src/components/Page/index.spec.js
@@ -3,7 +3,12 @@ import * as PublicApi from '.';
 
 const { Page } = PublicApi;
 
+jest.mock('react-helmet-async', () => ({
+  Helmet: (props) => <x-react-helmet-async {...props} />,
+}));
+
 jest.mock('components/Layout', () => ({
+  Header: (props) => <x-layout-header {...props} />,
   Layout: (props) => <x-layout-main {...props} />,
   Main: (props) => <x-layout-layout {...props} />,
 }));
@@ -21,6 +26,14 @@ describe('<Page/>', () => {
   test('renders properly', () => {
     expect(renderComponent().firstChild).toMatchInlineSnapshot(`
       <x-layout-main>
+        <x-react-helmet-async>
+          <link
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+        </x-react-helmet-async>
+        <x-layout-header />
         <x-layout-layout>
           CHILDREN
         </x-layout-layout>
