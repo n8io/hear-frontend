aa5df8f2a1663cc0feb33ba9812214c6e6617ac7
Nate Clark <n8@n8io.com>
2021-03-11 09:38:49 -0500

    🧪 Improve Snood provider test coverage

--- src/modules/app/App/Providers/Snood/index.spec.js
+++ src/modules/app/App/Providers/Snood/index.spec.js
@@ -1,9 +1,40 @@
+import { makeRenderComponent } from 'testHelpers';
+import { random } from 'utils/profileImage';
 import * as PublicApi from '.';
 
+jest.mock('hooks/useSnood', () => ({
+  Provider: (props) => <x-snood-provider {...props} />,
+}));
+
+jest.mock('utils/profileImage');
+
+const { Provider } = PublicApi;
+
+const defaultProps = { children: 'CHILDREN' };
+
+const renderComponent = makeRenderComponent({
+  component: Provider,
+  defaultProps,
+});
+
 describe('the Snood provider', () => {
+  beforeEach(() => {
+    random.mockReturnValue('snood_image_url');
+  });
+
   test('exports only the allowed exports', () => {
     const allowedExports = ['Provider'];
 
     expect(Object.keys(PublicApi).sort()).toEqual(allowedExports);
+  });
+
+  test('renders properly', () => {
+    expect(renderComponent().firstChild).toMatchInlineSnapshot(`
+      <x-snood-provider
+        value="snood_image_url"
+      >
+        CHILDREN
+      </x-snood-provider>
+    `);
   });
 });
