51b16658481d7e93f9082a20ca8e8583ce4466de
Nate Clark <n8@n8io.com>
2021-03-11 10:05:37 -0500

    ✨ Add two more grays to color palette

--- src/modules/app/App/GlobalStyles/colors.js
+++ src/modules/app/App/GlobalStyles/colors.js
@@ -4,6 +4,8 @@ const palette = [
   { key: 'white', value: '#fff' },
   { key: 'gray-100', value: '#f9f9f9' },
   { key: 'gray-300', value: '#dcdcdc' },
+  { key: 'gray-400', value: '#cacaca' },
+  { key: 'gray-500', value: '#b1b1b1' },
   { key: 'black', value: '#000' },
 ];
 
