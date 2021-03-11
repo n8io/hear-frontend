db11461a9dcd216452babbe0f3cfb7b11a5124cb
Nate Clark <n8@n8io.com>
2021-03-11 09:18:22 -0500

    💄 Style app header

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -2,17 +2,31 @@ import { Logo } from 'components/Logo';
 import { route } from 'constants/route';
 import { useSnood } from 'hooks/useSnood';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  align-items: center;
+  background: var(--color-white, #fff);
+  box-shadow: 0 1px 0.25rem var(--color-gray-300, #dcdcdc);
+  column-gap: 0.5rem;
+  display: grid;
+  grid-template-columns: auto 1fr;
+`;
+
+const StyledLink = styled(Link)`
+  height: 3rem;
+`;
 
 const Header = () => {
   const snood = useSnood();
 
   return (
-    <header>
-      <Link to={route.ROOT}>
-        <img height={32} src={snood} width={32} />
-        <Logo height={18} />
-      </Link>
-    </header>
+    <StyledHeader>
+      <StyledLink to={route.ROOT}>
+        <img alt="Reddit Snood character" height={48} src={snood} width={48} />
+      </StyledLink>
+      <Logo height={24} />
+    </StyledHeader>
   );
 };
 
